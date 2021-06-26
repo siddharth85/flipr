5729dd10a287e747aaca5e6a66770ed4af9c3d12
shankhanil007 <shankhanilborthakur@gmail.com>
2021-06-27 04:09:46 +0530

    Added Contacts through API

--- server/routes/addContact.js
+++ server/routes/addContact.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/User");
+
 router.post("/", async (req, res) => {
   try {
+    User.findById("60d752260b0a3453bcf18d22", function (err, details) {
+      if (err) console.log(err);
+      else {
+        const type = req.body.category;
+        const contacts = req.body.contacts;
+        const check = details.get(`category.${type}`);
+
+        if (check === undefined) {
+          details.set(`category.${type}`, contacts);
+          details.save();
+        } else {
+          contacts.forEach((data) => {
+            details.get(`category.${type}`).push(data);
+          });
+
+          details.save();
+        }
+      }
+    });
   } catch (err) {}
 });
 
