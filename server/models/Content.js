0c9cd92763b112041cf81df396c5eb3b36877226
shankhanil007 <shankhanilborthakur@gmail.com>
2021-06-27 12:36:44 +0530

    Send email on specific date

--- server/models/Content.js
+++ server/models/Content.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const ContentSchema = new mongoose.Schema({
   job_id: String,
+  name: String,
   subject: String,
   body: String,
   sent_to: { type: Array },
--- server/routes/agenda.js
+++ server/routes/agenda.js
@@ -14,11 +14,12 @@ const agenda = new Agenda({
 });
 
 agenda.define("SEND_MAIL", (job, done) => {
-  const { user_id, subject, body, sent_to } = job.attrs.data;
+  const { user_id, name, subject, body, sent_to } = job.attrs.data;
 
   Content.create(
     new Content({
       job_id: job.attrs._id,
+      name: name,
       subject: subject,
       body: body,
       sent_to: sent_to,
@@ -39,47 +40,26 @@ agenda.define("SEND_MAIL", (job, done) => {
 });
 
 router.post("/", async (req, res) => {
-  let { user_id, name, subject, body, sent_to, date, time, isRecurring } =
-    req.body;
+  let { user_id, name, subject, body, sent_to, date, isRecurring } = req.body;
 
-  var dateUTC = new Date(date);
-  // const yearUTC = dateUTC.getFullYear();
-  // const monthUTC = dateUTC.getMonth();
-  // const dayUTC = dateUTC.getDate();
-  // var newDate = new Date(yearUTC, monthUTC, dayUTC + 1);
-  // console.log(dateUTC);
-  // console.log(newDate);
-
-  dateUTC = dateUTC.getTime();
-  var dateIST = new Date(dateUTC);
-  //date shifting for IST timezone (+5 hours and 30 minutes)
-  dateIST.setHours(dateIST.getHours() + 5);
-  dateIST.setMinutes(dateIST.getMinutes() + 30);
-
-  console.log(dateIST);
-  console.log(time);
-
-  // console.log(dateUTC.getDate());
-  // const dateIST = new Date(getUTCDate(dateUTC));
-  // console.log(dateIST);
-
-  // if (isRecurring === false) {
-  //   try {
-  //     (async function () {
-  //       await agenda.start();
-  //       await agenda.schedule(date, "SEND_MAIL", {
-  //         user_id,
-  //         subject,
-  //         body,
-  //         sent_to,
-  //       });
-  //       return res.status(200).json({ msg: "Success" });
-  //     })();
-  //   } catch (err) {
-  //     console.error(err.message);
-  //     res.status(500).send("Server Error");
-  //   }
-  // }
+  if (isRecurring === false) {
+    try {
+      (async function () {
+        await agenda.start();
+        await agenda.schedule(date, "SEND_MAIL", {
+          user_id,
+          name,
+          subject,
+          body,
+          sent_to,
+        });
+        return res.status(200).json({ msg: "Success" });
+      })();
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
 });
 
 module.exports = router;
